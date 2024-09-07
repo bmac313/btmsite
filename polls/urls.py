177dd79d466492a1927c6f1511d8cfa55e2b8de9
BKM <brendan.t.mcrae@gmail.com>
2024-09-07 15:58:40 -0500

    Tutorial pt 1

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),                                # /polls/
+    path("<int:question_id>/", views.detail, name="detail"),            # /polls/{id}/
+    path("<int:question_id>/results/", views.results, name="results"),  # /polls/{id}/results
+    path("<int:question_id>/vote/", views.vote, name="vote")            # /polls/{id}/vote
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world, You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    response_text = "Viewing details for question %s."
+    return HttpResponse(response_text % question_id)
+
+def results(request, question_id):
+    response_text = "Viewing results of question &s."
+    return HttpResponse(response_text % question_id)
+
+def vote(request, question_id):
+    response_text = "Voting on question %s."
+    return HttpResponse(response_text % question_id)
