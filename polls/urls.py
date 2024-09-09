942288e06615403b2b1f93aa46e5d88fece338e3
BKM <brendan.t.mcrae@gmail.com>
2024-09-09 15:08:45 -0500

    tutorial progress

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),                                # /polls/
-    path("<int:question_id>/", views.detail, name="detail"),            # /polls/{id}/
-    path("<int:question_id>/results/", views.results, name="results"),  # /polls/{id}/results
-    path("<int:question_id>/vote/", views.vote, name="vote")            # /polls/{id}/vote
+    path("", views.index, name="index"),                                 # /polls/
+    path("<int:question_id>/", views.detail, name="detail"),             # /polls/{id}/
+    path("<int:question_id>/results/", views.results, name="results"),   # /polls/{id}/results
+    path("<int:question_id>/vote/", views.vote, name="vote")             # /polls/{id}/vote
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import HttpResponse, Http404
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+	template = loader.get_template("polls/index.html")
+	context = {
+		"latest_question_list": latest_question_list,
+	}
+	return render(request, "polls/index.html", context)
+	# above is equivalent to:
+	# return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    response_text = "Viewing details for question %s."
-    return HttpResponse(response_text % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/detail.html", {"question": question})
+	#above is equivalent to:
+	#try:
+	#	question = Question.objects.get(pk=question_id)
+	#except Question.DoesNotExist:
+	#	raise Http404("Question does not exist")
+	#	
+	#return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    response_text = "Viewing results of question &s."
-    return HttpResponse(response_text % question_id)
+	return HttpResponse("Viewing results for questions %s." % question_id)
 
 def vote(request, question_id):
-    response_text = "Voting on question %s."
-    return HttpResponse(response_text % question_id)
+	return HttpResponse("Voting on question %s." % question_id)
